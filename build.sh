e4371e3f8d2f53a358181446c12bccf9bb474ae1
Philip-Larsen-Donnelly <35666657+Philip-Larsen-Donnelly@users.noreply.github.com>
2019-02-11 14:53:03 +0100

    build: change build for localisation and new styling (#79)
    * refactor: continue to refactor and style in prep for localisation
    
    * fix: fix chunked template after refactor
    
    * fix: fix styles on chunked view
    
    * chore: add the GNU Free Documentation License file to the repository
    
    * build: localisation changes

--- build.sh
+++ build.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
+#
+# Requirements:
+#
+# ensure language packs are installed. e.g. for French
+#> sudo apt-get install language-pack-fr
+#
+
+# pushing docs to localisation platform (transifex) is only done on Jenkins
+LOCALISE=0
+if [[ `id -un` == "jenkins" ]]; then
+  # and only where configured
+  if [ -f ~/.transifexrc ]; then
+      LOCALISE=1
+  fi
+fi
+
+
 if [ ! -d "venv" ]; then
     source venv_setup
 fi
 
 source ./venv/bin/activate
 src="$PWD/src/commonmark/en"
-tmp="$PWD/tmp/en"
-target="$PWD/target/commonmark/en"
+TMPBASE="$PWD/tmp"
+tmp="$TMPBASE/en"
 localisation_root="$PWD/target/commonmark"
 
-rm -rf $tmp
-mkdir -p $tmp
-rm -rf $target
-mkdir -p $target
+rm -rf $TMPBASE
+mkdir -p $TMPBASE
+rm -rf $localisation_root
+mkdir -p $localisation_root
 
 shared_resources() {
     mkdir -p $1/resources/
@@ -35,6 +52,7 @@ assemble_resources() {
 }
 
 make_html() {
+    cd $TMPBASE/$lang
     name=$1
     subdir=$2
     mkdir -p ${target}/${subdir}/html
@@ -45,7 +63,13 @@ make_html() {
     done
     echo "compiling $name.md to html"
     chapters="bookinfo.md ${name}.md"
-    pandoc $chapters -c ./resources/css/dhis2.css --template="dhis2_template.html" --toc -N --section-divs -t html5 -o ${target}/${subdir}/html/${name}_full.html
+    css="./resources/css/dhis2.css"
+    template="./resources/templates/dhis2_template.html"
+    thanks=""
+    if [ -f "./resources/i18n/thanks/${name}_${lang}.html" ]; then
+        thanks=" -B ./resources/i18n/thanks/${name}_${lang}.html "
+    fi
+    pandoc ${thanks} ${chapters} -c ${css} --template="${template}" --toc -N --section-divs -t html5 -o ${target}/${subdir}/html/${name}_full.html
 
     cd ${target}/${subdir}/html/
     # fix the section mappings in the full html file
@@ -53,38 +77,35 @@ make_html() {
     id_mapper ${name}_full.html
     # split the full html file into chunks
     echo "splitting the html file into chunks"
-    chunker ${name}_full.html $tmp/dhis2_chunked_template.html
-    cd $tmp
+    chunked_template="$tmp/resources/templates/dhis2_chunked_template.html"
+    chunker ${name}_full.html ${chunked_template}
+
 
 }
 
 make_pdf() {
+    cd $TMPBASE/$lang
     name=$1
     subdir=$2
+    echo "making pdf in $PWD"
     mkdir -p ${target}/${subdir}
     echo "compiling $name.md to pdf"
     chapters="bookinfo.md ${name}.md"
-    pandoc $chapters -c ./resources/css/dhis2_pdf.css --template="dhis2_template.html" --toc -N --section-divs --pdf-engine=weasyprint -o ${target}/${subdir}/${name}.pdf
+    css="./resources/css/dhis2_pdf.css"
+    template="./resources/templates/dhis2_template.html"
+    thanks=""
+    if [ -f "./resources/i18n/thanks/${name}_${lang}.html" ]; then
+        thanks=" -B ./resources/i18n/thanks/${name}_${lang}.html "
+    fi
+    pandoc ${thanks} ${chapters} -c ${css} --template="${template}" --toc -N --section-divs --pdf-engine=weasyprint -o ${target}/${subdir}/${name}.pdf
 }
 
-generate(){
+assemble(){
     name=$1
-    subdir=$2
-    selection=$3
-    if [ ! $selection ]
-    then
-      selection="both"
-    fi
-
-    echo "+--------------------------------------------------------"
-    echo "| Processing: $name"
-    echo "+--------------------------------------------------------"
 
     # copy resources and assembled markdown files to temp directory
     shared_resources $tmp
-    #cp $src/${name}*.md $tmp/
-    cp $src/*template.html $tmp/
-    cp $src/content/common/* $tmp/
+    cp $src/content/common/bookinfo.md $tmp/
     cd $src
 
     # copy resources and assembled markdown files to temp directory
@@ -100,17 +121,64 @@ generate(){
       done
     done
 
-    # go to the temp directory and build the documents - put output in target directory
-    cd $tmp
+}
+
+update_localizations(){
+    name=$1
+    cd $TMPBASE
+
+
+    # check for the transifex environment
+    if [ ${LOCALISE} ]; then
+       mkdir -p .tx
+       cp en/resources/i18n/transifex-config .tx/config
+       # <tx-project>.<resource-name>
+       txproject=`git rev-parse --abbrev-ref HEAD`
+       sed -i "s/<tx-project>/${txproject//.}/" .tx/config
+       sed -i "s/<name>/${name}/" .tx/config
+       sed -i "s/<resource-name>/${name//_/-}/" .tx/config
+       # Use "prettier" to ensure each markdown text block is on a single line
+       # This is necessary to ensure that blocks are correctly parsed by transifex.
+       prettier --prose-wrap never --write en/dhis2_end_user_manual.md
+
+       # Push source updates to transifex
+       tx push -s
+
+       # Pull the translations from transifex
+       # only French at the moment - should be refactored for multiple languages
+       tx pull -l fr
+       ln -s ../en/resources fr/resources
+
+
+    else
+       echo "INFO: Transifex not configured. Localised versions will not be generated."
+    fi
+
+  }
+
+build_docs(){
+    name=$1
+    subdir=$2
+    selection=$3
+    lang=$4
+    locale=$5
+
+    cd $TMPBASE/$lang
+    echo "Working in $PWD"
+
+    target="$localisation_root/$lang"
+    mkdir -p $target
 
     gitbranch=`git rev-parse --abbrev-ref HEAD`
     githash=`git rev-parse --short HEAD`
     gitdate=`git show -s --format=%ci $githash`
-    gityear=`date -d "${gitdate}" +%Y`
+    gityear=`date -d "${gitdate}" '+%Y'`
+    gitmonth=`LC_TIME=${locale}.utf8 date -d "${gitdate}" '+%B'`
     sed -i "s/<git-branch>/$gitbranch/" bookinfo*.md
     sed -i "s/<git-hash>/$githash/" bookinfo*.md
     sed -i "s/<git-date>/$gitdate/" bookinfo*.md
     sed -i "s/<git-year>/$gityear/" bookinfo*.md
+    sed -i "s/<git-month>/$gitmonth/" bookinfo*.md
 
     if [ $selection == "html" ]
     then
@@ -125,15 +193,38 @@ generate(){
 
 }
 
-# for r in `find . -type f | grep "resources/images"`; do
-#     #echo "resource: $r"
-#     assemble_resources $r $tmp
-# done
+generate(){
+    name=$1
+    subdir=$2
+    selection=$3
+    if [ ! $selection ]
+    then
+      selection="both"
+    fi
+
+    echo "+--------------------------------------------------------"
+    echo "| Processing: $name"
+    echo "+--------------------------------------------------------"
+
+    assemble $name
+    update_localizations $name
+
+    # go to the temp directory and build the documents - put output in target directory
+    build_docs $name $subdir $selection en en_UK
+
+    # build localised versions
+    if [ ${LOCALISE} ]; then
+        build_docs $name $subdir $selection fr fr_FR
+    fi
+
+
+
+}
 
 
 # comment as you wish
 # format:
-# $> generate <doc name> <chapters subfolder> ["html","pdf","both"]
+#$> generate <doc name> <chapters subfolder> ["html","pdf","both"]
 generate "dhis2_android_user_man" "android"
 generate "dhis2_developer_manual" "developer"
 generate "dhis2_user_manual_en" "user"
