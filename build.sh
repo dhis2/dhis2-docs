a3340a370ecc62578caa8d38efd30970ef5b350e
Philip-Larsen-Donnelly <35666657+Philip-Larsen-Donnelly@users.noreply.github.com>
2019-07-12 23:03:32 +0200

    MacOS build support (#178)
    * chore: add pandoc libraries for mac
    
    * fix: exit if python3 not available
    
    * fix: exit if coreutils not available
    
    * fix: exit if gnu-sed not available
    
    * fix: security update
    
    * refactor: let weasyprint install its requirements
    
    * refactor: and set default locale
    
    * chore: zip pandoc files
    
    * chore: update directory tree

--- build.sh
+++ build.sh
@@ -3,9 +3,36 @@
 #
 # Requirements:
 #
+# ensure python3 is available:
+if [[ ! $(command -v python3) ]]; then
+  echo "This script requires python3, and some other libraries:"
+  echo "  brew install python3 cairo pango gdk-pixbuf libffi"
+  echo "Exiting."
+  exit 0
+fi
+#
+# If on mac, ensure we have gnu coreutils for command compatibility
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  if [[ ! $(command -v gdate) ]] || [[ ! $(command -v gsed) ]]; then
+    echo "This script requires coreutils and gnu-sed to be installed on Mac:"
+    echo "  brew install coreutils gnu-sed"
+    echo "Exiting."
+    exit 0
+  fi
+  # once gnu coreutils and gnu-sed are installed, we can ensure they are
+  # first in the path, for convenience
+  corepath="/usr/local/opt/coreutils/libexec/gnubin"
+  sedpath="/usr/local/opt/gnu-sed/libexec/gnubin"
+  export PATH="$sedpath:$corepath:$PATH"
+fi
+#
 # ensure language packs are installed. e.g. for French
 #> sudo apt-get install language-pack-fr
 #
+
+# set the default locale for the build
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
 
 # perform all actions relative to the path of this script
 SCRIPT_DIR="${BASH_SOURCE%/*}"
