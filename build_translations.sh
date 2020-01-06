0c218d75f0aea76c26631e9eec12db08e7191601
Philip-Larsen-Donnelly <35666657+Philip-Larsen-Donnelly@users.noreply.github.com>
2020-01-06 01:28:18 +0100

    refactor: align home page (#327)

--- build_translations.sh
+++ build_translations.sh
@@ -7,12 +7,7 @@ git checkout master -- lib
 git checkout master -- venv_setup
 git checkout master -- requirements.txt
 
-#
-# Requirements:
-#
-# ensure language packs are installed. e.g. for French
-#> sudo apt-get install language-pack-fr
-#
+
 
 # perform all actions relative to the path of this script
 SCRIPT_DIR="${BASH_SOURCE%/*}"
@@ -23,58 +18,12 @@ else
   SCRIPT_DIR="$PWD"
 fi
 
-# pushing docs to localisation platform (transifex) is only done on Jenkins
-LOCALISE=0
-if [[ `id -un` == "jenkins" ]]; then
-  # and only where configured
-  if [ -f ~/.transifexrc ]; then LOCALISE=1; fi
-fi
 
-# set up the python environment
-if [ ! -d "venv" ]; then
-    source venv_setup
-fi
-source ./venv/bin/activate
-
-# script variables
-src="$SCRIPT_DIR/src/commonmark/en"
-TMPBASE="$SCRIPT_DIR/tmp"
-tmp="$TMPBASE/en"
-localisation_root="$SCRIPT_DIR/target/commonmark"
-
-# clear the output directories
-rm -rf $TMPBASE
-mkdir -p $TMPBASE
-rm -rf $localisation_root
-mkdir -p $localisation_root
 
 # include helper functions
 . "$SCRIPT_DIR/lib/doc_functions.sh"
 
-# translate function called for each document
-translate(){
-    name=$1
-    subdir=$2
-    selection=$3
-    lang=$4
-    locale=$5
 
-    if [ ! $selection ]
-    then
-      selection="both"
-    fi
-
-    echo "+--------------------------------------------------------"
-    echo "| Processing: $name"
-    echo "+--------------------------------------------------------"
-
-    # we need to assemble the source documents to consolidate the resources
-    assemble $name
-
-    pull_translations $name
-    # build localised versions
-    build_docs $name $subdir $selection $lang $locale
-}
 
 # build localised versions
 if [ ${LOCALISE} -eq 1  ]; then
@@ -115,13 +64,7 @@ if [ ${LOCALISE} -eq 1  ]; then
         translate "dhis2_developer_manual" "developer" "both" $lang $locale
         translate "dhis2_android_sdk_user_guide" "android-sdk" "both" $lang $locale
 
-        pushd $tmp
-          pushd docs
-            ln -s ../resources .
-          popd
-          rm -rf resources/mkdocs
-          mkdocs build --dirty
-        popd
+        make_mkdocs
 
 
     done
