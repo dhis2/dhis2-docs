916e8503bab3b72955d22f52742179fcc14f576a
Philip-Larsen-Donnelly <35666657+Philip-Larsen-Donnelly@users.noreply.github.com>
2020-01-28 19:47:38 +0100

    chore: integrate android implementation guidelines (#375)

--- build.sh
+++ build.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# build resources from the master branch
+# we use the build resources from the master branch
 git checkout master -- src/commonmark/en/resources
 git checkout master -- tools
 git checkout master -- lib
 git checkout master -- venv_setup
 git checkout master -- requirements.txt
-
-
 
 # perform all actions relative to the path of this script
 SCRIPT_DIR="${BASH_SOURCE%/*}"
@@ -19,11 +17,8 @@ else
 fi
 
 
-
 # include helper functions
 . "$SCRIPT_DIR/lib/doc_functions.sh"
-
-
 
 
 # comment as you wish
@@ -39,6 +34,7 @@ generate "dhis2_scorecard_manual" "scorecard-app"
 
 echo "    - Implementer:" >> $myml
 generate "dhis2_implementation_guide" "implementer"
+generate "dhis2_android_implementation_guideline" "implementer"
 generate "dhis2_android_capture_app" "android-app"
 generate "user_stories_book" "user-stories"
 
