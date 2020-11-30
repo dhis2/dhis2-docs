ff952904ab84d59774817d469eff49979279702a
YuryRog <58734904+YuryRog@users.noreply.github.com>
2020-11-30 22:48:21 +0100

    docs: Update build.sh (#564)
    * feat: Update build.sh
    
    * Update build.sh

--- build.sh
+++ build.sh
@@ -50,7 +50,9 @@ generate "dhis2_system_administration_guide" "sysadmin"
 echo "    - Metadata:" >> $myml
 generate "dhis2_who_digital_health_data_toolkit" "metadata"
 generate "dhis2_covid19_surveillance" "packages"
+generate "dhis2_IDSR" "packages"
 generate "dhis2_immunisation" "packages"
+generate "dhis2_RMNCAH" "packages"
 generate "dhis2_malaria" "packages"
 generate "dhis2_tb_surveillance" "packages"
 
